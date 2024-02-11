2d7d1b274fb793e72b111f1ae2fd40a5317ca9a5
G <jos-ger@users.noreply.huggingface.co>
2024-02-11 00:10:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import gradio as gr
-from transformers import pipeline
+import sklearn
+import pandas pd
+import numpy as np
 
-pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
-
-def predict(input_img):
-    predictions = pipeline(input_img)
-    return input_img, {p["label"]: p["score"] for p in predictions} 
-
-gradio_app = gr.Interface(
-    predict,
-    inputs=gr.Image(label="Select hot dog candidate", sources=['upload', 'webcam'], type="pil"),
-    outputs=[gr.Image(label="Processed Image"), gr.Label(label="Result", num_top_classes=2)],
-    title="Hot Dog? Or Not?",
-)
-
-if __name__ == "__main__":
-    gradio_app.launch()
+def make_prediction(gender, Partner, Dependents, tenure, MultipleLines,
+       InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,
+       TechSupport, Contract, PaperlessBilling, PaymentMethod,
+       MonthlyCharges, TotalCharges):
+   input_data = pd.DataFrame({'gender':[gender], 'Partner':[Partner], 'Dependents':[Dependents], 'tenure':[tenure], 'MultipleLines':[MultipleLines],
+       'InternetService':[InternetService], 'OnlineSecurity':[OnlineSecurity], 'OnlineBackup':[OnlineBackup], 'DeviceProtection':[DeviceProtection],
+       'TechSupport':[TechSupport], 'Contract':[Contract], 'PaperlessBilling':[PaperlessBilling], 'PaymentMethod':[PaymentMethod],
+       'MonthlyCharges':[MonthlyCharges], 'TotalCharges':[TotalCharges]})
+   
+   #load already saved pipeline and make predictions
+    with open("ml_model.pkl", "rb") as f:
+        model = pickle.load(f)
+        predt = model.predict(input_data) 
+    #return prediction 
+    return predt

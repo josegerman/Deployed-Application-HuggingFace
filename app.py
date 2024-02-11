04f6ac8565771aa0345639a176d09e765b565021
G <jos-ger@users.noreply.huggingface.co>
2024-02-11 03:41:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,36 @@ import sklearn
 import pandas pd
 import numpy as np
 
-def make_prediction(gender, Partner, Dependents, tenure, MultipleLines,
-       InternetService, OnlineSecurity, OnlineBackup, DeviceProtection,
-       TechSupport, Contract, PaperlessBilling, PaymentMethod,
-       MonthlyCharges, TotalCharges):
-   input_data = pd.DataFrame({'gender':[gender], 'Partner':[Partner], 'Dependents':[Dependents], 'tenure':[tenure], 'MultipleLines':[MultipleLines],
-       'InternetService':[InternetService], 'OnlineSecurity':[OnlineSecurity], 'OnlineBackup':[OnlineBackup], 'DeviceProtection':[DeviceProtection],
-       'TechSupport':[TechSupport], 'Contract':[Contract], 'PaperlessBilling':[PaperlessBilling], 'PaymentMethod':[PaymentMethod],
-       'MonthlyCharges':[MonthlyCharges], 'TotalCharges':[TotalCharges]})
+def make_prediction(LotFrontageSF, LotAreaSF, BldgType, HouseStyle,
+                   OverallQual, OverallCond, YearBuilt, YearRemodAdd,
+                   RoofStyle, RoofMaterial, ExterQual, ExterCond,
+                   Foundation, BsmtQual, BsmtCond, BsmtFinSF, TotalBsmtSF,
+                   Heating, HeatingQC, CentralAir, 1stFlrSF, 2ndFlrSF,
+                   GrLivArea, GarageType, GarageYrBlt, GarageCars, GarageSF,
+                   GarageQual, WoodDeckSF, OpenPorchSF, MiscFeature):
+    input_data = pd.DataFrame({'LotFrontageSF': [LotFrontageSF], 'LotAreaSF': [LotAreaSF], 'BldgType': [BldgType], 'HouseStyle': [HouseStyle],
+                               'OverallQual': [OverallQual], 'OverallCond': [OverallCond], 'YearBuilt': [YearBuilt], 'YearRemodAdd': [YearRemodAdd],
+                               'RoofStyle': [RoofStyle], 'RoofMaterial': [RoofMaterial], 'ExterQual': [ExterQual], 'ExterCond': [ExterCond],
+                               'Foundation': [Foundation], 'BsmtQual': [BsmtQual], 'BsmtCond': [BsmtCond], 'BsmtFinSF': [BsmtFinSF], 'TotalBsmtSF': [TotalBsmtSF],
+                               'Heating': [Heating], 'HeatingQC': [HeatingQC], 'CentralAir': [CentralAir], '1stFlrSF': [1stFlrS], '2ndFlrSF': [2ndFlrSF],
+                               'GrLivArea': [GrLivArea], 'GarageType': [GarageType], 'GarageYrBlt': [GarageYrBlt], 'GarageCars': [GarageCars], 'GarageSF': [GarageSF],
+                               'GarageQual': [GarageQual], 'WoodDeckSF': [WoodDeckSF], 'OpenPorchSF': [OpenPorchSF], 'MiscFeature': [MiscFeature]})
+
+           
    
-   #load already saved pipeline and make predictions
-    with open("ml_model.pkl", "rb") as f:
-        model = pickle.load(f)
-        predt = model.predict(input_data) 
-    #return prediction 
-    return predt
+#load already saved pipeline and make predictions
+with open("house_price_api.pkl", "rb") as f:
+    model = pickle.load(f)
+    predt = model.predict(input_data) 
+#return prediction 
+return predt
+
+#create the input components for gradio
+LotFrontageSF_input = gr.number()
+LotAreaSF_input = gr.number()
+BldgType_input = gr.inputs.Drowdown(choices =['Single Family Detached', 'Duplex'])
+HouseStyle_input = gr.inputs.Drowdown(choices =['1 Story', '2 Story']
+
+
+
+output = gr.Textbox(label='Prediction') 

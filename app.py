63f1d13c83ae5b349a3fc31ed976e40e5014b8b2
JoseG <josegerman@users.noreply.github.com>
2024-02-12 18:39:58 -0500

    update to app

--- app.py
+++ app.py
@@ -1,26 +1,80 @@
-import gradio as gr
-#import sklearn 
-import pycaret
+# Import required libraries
+
 import pandas as pd
 import numpy as np
 import pickle
+import gradio
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
+from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.preprocessing import LabelEncoder
+import warnings
+warnings.filterwarnings('ignore')
 
-def make_prediction(LotFrontageSF, LotAreaSF, BldgType, HouseStyle,
-                   OverallQual, OverallCond, YearBuilt, YearRemodAdd,
-                   RoofStyle, RoofMaterial, ExterQual, ExterCond,
-                   Foundation, BsmtQual, BsmtCond, BsmtFinSF, TotalBsmtSF,
-                   Heating, HeatingQC, CentralAir, FirstFlrSF, SecondFlrSF,
-                   GrLivArea, GarageType, GarageYrBlt, GarageCars, GarageSF,
-                   GarageQual, WoodDeckSF, OpenPorchSF, MiscFeature):
-    input_data = pd.DataFrame( )
+# Load saved model using pickle
+with open('randomforestregressor_model.pkl', 'rb') as file1:
+    model1 = pickle.load(file1)
 
-    #load already saved pipeline and make predictions
-    with open("houseprice_best_model.pkl", "rb") as f:
-        model = pickle.load(f)
-        print(model)
-        #predt = model.predict(input_data) 
-    #return prediction 
-    #return predt
+# Load saved encoder objects using pickle
+with open('BldgType_le.pkl','rb') as f1:
+    BldgType_le = pickle.load(f1)
+
+with open('HouseStyle_le.pkl','rb') as f2:
+    HouseStyle_le = pickle.load(f2)
+
+with open('RoofStyle_le.pkl','rb') as f3:
+    RoofStyle_le = pickle.load(f3)
+
+with open('RoofMaterial_le.pkl','rb') as f4:
+    RoofMaterial_le = pickle.load(f4)
+
+with open('Foundation_le.pkl','rb') as f5:
+    Foundation_le = pickle.load(f5)
+
+with open('Heating_le.pkl','rb') as f6:
+    Heating_le = pickle.load(f6)
+
+with open('MiscFeature_le.pkl','rb') as f7:
+    MiscFeature_le = pickle.load(f7)
+
+with open('CentralAir_le.pkl','rb') as f8:
+    CentralAir_le = pickle.load(f8)
+
+with open('GarageType_le.pkl','rb') as f9:
+    GarageType_le = pickle.load(f9)
+
+# Define prediction function
+def predict_house_price(LotFrontageSF,LotAreaSF,OverallQual,OverallCond,YearBuilt,
+                        YearRemodAdd,ExterQual,ExterCond,BsmtQual,BsmtCond,
+                        BsmtFinSF,TotalBsmtSF,HeatingQC,FirstFlrSF,SecondFlrSF,
+                        GrLivArea,GarageYrBlt,GarageCars,GarageSF,GarageQual,
+                        WoodDeckSF,OpenPorchSF,BldgType,HouseStyle,RoofStyle,
+                        RoofMaterial,Foundation,Heating,MiscFeature,CentralAir,
+                        GarageType):
+    
+    BldgType_encoded = BldgType_le.transform([BldgType])[0]
+    HouseStyle_encoded = HouseStyle_le.transform([HouseStyle])[0]
+    RoofStyle_encoded = RoofStyle_le.transform([RoofStyle])[0]
+    RoofMaterial_encoded = RoofMaterial_le.transform([RoofMaterial])[0]
+    Foundation_encoded = Foundation_le.transform([Foundation])[0]
+    Heating_encoded = Heating_le.transform([Heating])[0]
+    MiscFeature_encoded = MiscFeature_le.transform([MiscFeature])[0]
+    CentralAir_encoded = CentralAir_le.transform([CentralAir])[0]
+    GarageType_encoded = GarageType_le.transform([GarageType])[0]
+    
+    
+
+    input_data = pd.DataFrame({'LotFrontageSF': [LotFrontageSF], 'LotAreaSF': [LotAreaSF], 'OverallQual': [OverallQual], 'OverallCond': [OverallCond], 'YearBuilt': [YearBuilt],
+                               'YearRemodAdd': [YearRemodAdd], 'ExterQual': [ExterQual], 'ExterCond': [ExterCond], 'BsmtQual': [BsmtQual], 'BsmtCond': [BsmtCond],
+                               'BsmtFinSF': [BsmtFinSF], 'TotalBsmtSF': [TotalBsmtSF], 'HeatingQC': [HeatingQC],  'FirstFlrSF': [FirstFlrSF], 'SecondFlrSF': [SecondFlrSF],
+                               'GrLivArea': [GrLivArea],  'GarageYrBlt': [GarageYrBlt], 'GarageCars': [GarageCars], 'GarageSF': [GarageSF], 'GarageQual': [GarageQual],
+                               'WoodDeckSF': [WoodDeckSF], 'OpenPorchSF': [OpenPorchSF], 'BldgType_Encoded': [BldgType_encoded], 'HouseStyle_Encoded': [HouseStyle_encoded], 'RoofStyle_Encoded': [RoofStyle_encoded],
+                               'RoofMaterial_Encoded': [RoofMaterial_encoded], 'Foundation_Encoded': [Foundation_encoded], 'Heating_Encoded': [Heating_encoded], 'MiscFeature_Encoded': [MiscFeature_encoded], 'CentralAir_Encoded': [CentralAir_encoded],
+                               'GarageType_Encoded': [GarageType_encoded]})
+    
+    predict_house_price = model1.predict(input_data)
+
+    return predict_house_price[0]
 
 #create the input components for gradio
 LotFrontageSF_input = gr.Number(label='Lot Frontage (Squared Feet)',value = 1000)
@@ -58,37 +112,38 @@ output = gr.Textbox(label='Predicted House Price')
 
 
 #create the interface component
-app = gr.Interface(fn =make_prediction,inputs =[LotFrontageSF_input,
+app = gr.Interface(fn = predict_house_price,inputs = [LotFrontageSF_input,
                                                 LotAreaSF_input,
-                                                BldgType_input,
-                                                HouseStyle_input,
                                                 OverallQual_input,
                                                 OverallCond_input,
                                                 YearBuilt_input,
                                                 YearRemodAdd_input,
-                                                RoofStyle_input,
-                                                RoofMaterial_input,
                                                 ExterQual_input,
                                                 ExterCond_input,
-                                                Foundation_input,
                                                 BsmtQual_input,
                                                 BsmtCond_input,
                                                 BsmtFinSF_input,
                                                 TotalBsmtSF_input,
-                                                Heating_input,
                                                 HeatingQC_input,
-                                                CentralAir_input,
                                                 FirstFlrSF_input,
                                                 SecondFlrSF_input,
                                                 GrLivArea_input,
-                                                GarageType_input,
                                                 GarageYrBlt_input,
                                                 GarageCars_input,
                                                 GarageSF_input,
                                                 GarageQual_input,
                                                 WoodDeckSF_input,
                                                 OpenPorchSF_input,
-                                                MiscFeature_input],
+                                                BldgType_input,
+                                                HouseStyle_input,
+                                                RoofStyle_input,
+                                                RoofMaterial_input,
+                                                Foundation_input,
+                                                Heating_input,
+                                                MiscFeature_input,
+                                                CentralAir_input,
+                                                GarageType_input
+                                                ],
                    title ="House Price Generator",
                        description="Enter House details below and click the submit button to generate house price",
                    outputs = output)
